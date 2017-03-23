cd6415e961f7c0a5358b364a8d61a9cca662dbd4
willwangpraxent <william.wang@praxent.com>
2017-03-22 23:39:30 -0500

    b4

--- src/com/spstudio/test/HelloBetaCat.java
+++ src/com/spstudio/test/HelloBetaCat.java
@@ -3,11 +3,9 @@ package com.spstudio.test;
 public class HelloBetaCat {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("b3");
+		System.out.println("b4");
 	}
+	
+	
 
 }
