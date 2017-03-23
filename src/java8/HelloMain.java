c96cb7ae8b6c3e960fb6e7fe086eefaca705a917
willwangpraxent <william.wang@praxent.com>
2017-03-23 12:35:58 -0500

    m1

--- src/java8/HelloMain.java
+++ src/java8/HelloMain.java
@@ -3,16 +3,11 @@ package java8;
 public class HelloMain {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello, the world");
 		System.out.println("master2");
 		System.out.println("b1");
 		System.out.println("M1");
-=======
 		System.out.println("1");
-		// 2
-		// 3
->>>>>>> refs/remotes/origin/b1
 	}
 
 }
