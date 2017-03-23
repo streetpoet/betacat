d4e2bd066b89324409d6ab7dcdae271c9929e5cc
willwangpraxent <william.wang@praxent.com>
2017-03-23 11:30:00 -0500

    11

--- src/java8/HelloMain.java
+++ src/java8/HelloMain.java
@@ -4,7 +4,7 @@ public class HelloMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, the world");
-		System.out.println("master");
+		System.out.println("master2");
 	}
 
 }
